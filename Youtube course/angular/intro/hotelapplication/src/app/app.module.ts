b0778d1273f12d09bde277ce4b156b9126eeabcb
imadbenmadi <benmadi.imed@gmail.com>
2026-04-14 13:20:37 +0200

    .

--- Youtube course/angular/intro/hotelapplication/src/app/app.module.ts
+++ Youtube course/angular/intro/hotelapplication/src/app/app.module.ts
@@ -3,16 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RoomsModule } from './rooms/rooms.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, RoomsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ Youtube course/angular/intro/hotelapplication/src/app/rooms/rooms.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { RoomsComponent } from './rooms.component';
+
+@NgModule({
+  declarations: [RoomsComponent],
+  imports: [CommonModule],
+  exports: [RoomsComponent],
+})
+export class RoomsModule {}
